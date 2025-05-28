8ca8e69ef108cf1c63d53bde292a96bc338d655d
Millie99-thn <thethtetnyein9999@gmail.com>
2025-05-28 13:34:58 +0630

    Add files via upload

---
+++ thn_sequence.sql
@@ -0,0 +1,11 @@
+-- For item_id
+CREATE SEQUENCE seq_thn_item_id START WITH 1001 INCREMENT BY 1;
+
+-- For vendor_id
+CREATE SEQUENCE seq_thn_vendor_id START WITH 201 INCREMENT BY 1;
+
+-- For purchase order (po_id)
+CREATE SEQUENCE seq_thn_po_id START WITH 5001 INCREMENT BY 1;
+
+-- For PO item ID
+CREATE SEQUENCE seq_thn_po_item_id START WITH 10001 INCREMENT BY 1;
